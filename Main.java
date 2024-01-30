7389ac2c64ebc32b21e99fd0414cee576264bca2
Tanzilur31 <156154754+Zilu45@users.noreply.github.com>
2024-01-30 13:32:51 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,28 +1,11 @@
 
-package Konstruktor;
+package com.mycompany.modifier;
 
 public class Main {
-    public String username;
-    public String password;
-    
-    public Main(String username, String password){
-        this.username = username;
-        this.password = password;
-    }
     public static void main(String[] args) {
-        Main a = new Main("Neuro", "kopi");
-        System.out.println("Username: " + a.username);
-        System.out.println("Password: " + a.password);
+        
+    Person mPerson = new Person();
+    mPerson.setName("Zilu");
+        System.out.println("My Name: " + mPerson.getName());
     }
-       
-    User dian = new User();{
-
-// menggunakan method setter
-dian.setUsername("Neuro");
-dian.setPassword("Koentji Jawa");
-
-// menggunakan method getter
-System.out.println("Username: " + dian.getUsername());
-System.out.println("Password: " + dian.getPassword());
- }
 }
