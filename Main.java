84fe1a1ad71f5baf21e6c3d7e9dd48cdb892d365
Tanzilur31 <156154754+Zilu45@users.noreply.github.com>
2024-01-30 11:30:01 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.modifier;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    
+    Person mPerson = new Person();
+    mPerson.setName("Petani Kode");
+        System.out.println("Person Name: " + mPerson.getName());
+    }
+}
